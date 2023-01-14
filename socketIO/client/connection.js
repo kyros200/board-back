e494897b7eda9be0bef426111f67b62986857a77
rafael najjar <rafaelnajjar@gmail.com>
2023-01-14 03:22:44 -0300

    refactored a bit. now there is rooms, chat is working in basic form

--- socketIO/client/connection.js
+++ socketIO/client/connection.js
@@ -1,16 +1,31 @@
-const connection = (client) => {
-    client.on("ping", () => {
-        client.broadcast.emit("pong_public")
-        client.emit("pong_private")
-    });
+const { getRoomUsers } = require('../helpers/room')
+const { updateCount } = require('../helpers/update')
 
-    client.on('disconnecting', () => {
-    console.log(`User DISCONNECTING with ID: ${client.id}`)
-    });
+const connection = (io, client, info) => {
+    // client.on('disconnecting', () => {
+    // console.log(`User DISCONNECTING with ID: ${client.id}`)
+    // remove from rooms? chat to say its logging out?
+    // });
 
     client.on('disconnect', () => {
-        console.log(`User DISCONNECTED with ID: ${client.id}`)
+        updateCount(io, client, info)
+
+        const userInfo = info.loggedUsers.find(user => user.id === client.id)
+
+        info.loggedUsers = info.loggedUsers.filter(user => user.id != client.id)
+
+        console.log(`User "${userInfo.user}" DISCONNECTED (${client.id})`)
     });
+
+    client.on('getServerUsersCount', () => {
+        client.emit("getServerUsersCount", info.loggedUsers.length)
+    })
+
+    client.on('getRoomUsersCount', () => {
+        const userInfo = info.loggedUsers.find(user => user.id === client.id)
+
+        io.to(userInfo.room).emit("getRoomUsersCount", getRoomUsers(userInfo.room, info))
+    })
 }
 
 module.exports = {connection};
--- socketIO/index.js
+++ socketIO/index.js
@@ -1,5 +1,9 @@
 const {Server} = require('socket.io');
 const { connection } = require('./client/connection')
+const { clientInfo } = require('./client/clientInfo')
+const { clientActions } = require('./client/clientActions')
+const { chat } = require('./projects/chat/chat')
+const { updateCount } = require('./helpers/update')
 
 const startIO = (server) => {
     const io = new Server(server, {
@@ -7,11 +11,39 @@ const startIO = (server) => {
             origin: `http://localhost:3000`
         }
     });
+
+    const info = {
+        loggedUsers: []
+    };
+
+    io.use((client, next) => {
+        const clientQuery = client.handshake.query
+        console.log(`login attempt: u=${clientQuery.user} p=${clientQuery.pass} ...`)
+        if (clientQuery.pass != "1") {
+            console.log(`failed`)
+            next(new Error("User or Password incorrect! Please try Again"));
+        } else {
+            console.log("success! Going to connect...")
+            next()
+        }
+    })
     
     io.on("connection", (client) => {
-        console.log(`User Connected with ID: ${client.id}`)
+        const userQuery = client.handshake.query;
+        console.log(`User "${userQuery.user}" Connected to the Server (${client.id})`)
+        info.loggedUsers.push({
+            // loggedAt: new Date(),
+            id: client.id,
+            user: userQuery.user,
+            room: userQuery.room
+        })
+        updateCount(io, client, info)
 
-        connection(client)
+        connection(io, client, info)
+        clientInfo(io, client, info)
+        clientActions(io, client, info)
+        
+        chat(io, client, info)
     });
 
 }
